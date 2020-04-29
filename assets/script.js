ec06b3eb349b564855509f96f4a30dc792151774
Zerrtry <Zerrtry@icloud.com>
2020-04-28 23:44:59 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+const questions = [
+    {   title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {   title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    }
+];
+
+
+$(function () {
+    
+    let time = $('#time');
+    let start = $('#start');
+    let startBtn = $('#start-btn');
+    let question = $('#question');
+    let questionTitle = $ ('#question-title');
+    let answerBtn1 = $ ('#answer-btn1');
+    let answerBtn2 = $ ('#answer-btn2');
+    let answerBtn3 = $ ('#answer-btn3');
+    let answerBtn4 = $ ('#answer-btn4');
+    let end = $('#end');
+    let final = $('#final');
+    let finalSubmit = $('#finalSubmit');
+    let finalInput = $('#finalInput');
+    let score = $('#score');
+    let finalInputHistory = $('#finalInputHistory');
+    let finalHistory = $('#finalHistory');
+    let claenBtn = $('#Clean-btn');
+
+
+
+    //history
+     //<button type="button" class ="btn" id="delete-btn">delete</button>
+    //start quiz
+
+    //finish quiz
+   
+});
